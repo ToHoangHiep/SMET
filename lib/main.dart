2668dc3c21c03a693f449452e5876c0a43456d4a
phạm quang huy <huypqhe172172@fpr.edu.vn>
2026-03-06 21:43:36 +0700

    Divide the UI into smaller widgets for easier management.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:smet/page/login/login.dart';
 import 'package:smet/router/app_router.dart';
 import 'package:go_router/go_router.dart';
-import 'package:smet/page/admin_dashboard/user_management/user_management.dart';
+import 'package:smet/page/admin_dashboard/user_management/screen/user_management.dart';
 import 'package:smet/page/admin_dashboard/department_management/department_management.dart';
 
 void main() {
